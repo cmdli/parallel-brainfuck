4dfa6c761ee7b90e0ddd39faf216a0fb10953107
Chris de la Iglesia <cmdli@utexas.edu>
2013-11-22 12:46:51 -0600

    Reverted last commit; overlapped with sync branch

--- scala/src/Parser.scala
+++ scala/src/Parser.scala
@@ -2,7 +2,6 @@
  * Parses a program in BK
  */
 import scala.util.parsing.combinator._
-import scala.collection.immutable.LinearSeq
 
 abstract class Operation()
 case class AddOperation() extends Operation()
@@ -11,14 +10,9 @@ case class PrintOperation() extends Operation()
 case class InputOperation() extends Operation()
 case class ShiftRightOperation() extends Operation()
 case class ShiftLeftOperation() extends Operation()
-case class LoopOperations() extends Operation() {
-    var pair:LoopOperations = null
-    def set(newPair: LoopOperations) {
-        pair = newPair
-    }
+case class LoopOperations(ops: List[Operation]) extends Operation() {
+    def operations = ops
 }
-case class StartLoopOperation() extends LoopOperations()
-case class EndLoopOperation() extends LoopOperations()
 case class ForkOperation() extends Operation()
 case class InvalidOperation() extends Operation()
 
@@ -29,23 +23,20 @@ class Parser extends RegexParsers {
 
     def program: Parser[List[List[Operation]]] = rep(line)
 
-    def line: Parser[List[Operation]] = (block ~ "\n".?) ^^ {
-        case b:(~[List[Operation],Option[String]]) => b._1
+    def line: Parser[List[Operation]] = block ~ "\n" ^^ {
+        case block ~ "\n" => block
     }
 
     //Block of code
-    def block: Parser[List[Operation]] = {
-        println("Parsing block...")
-        (loop | char).*
-    }
+    def block: Parser[List[Operation]] = rep(loop | char)
 
     //A single char
     def char: Parser[Operation] = ("+" | "-" | "." | "," | ">" | "<" | "f") ^^ {
         case "+" => new AddOperation()
         case "-" => new SubOperation()
-        case "." =>new PrintOperation()
+        case "." => new PrintOperation()
         case "," => new InputOperation()
-        case ">" =>new ShiftRightOperation()
+        case ">" => new ShiftRightOperation()
         case "<" => new ShiftLeftOperation()
         case "f" => new ForkOperation()
         case _ => new InvalidOperation()
@@ -53,13 +44,7 @@ class Parser extends RegexParsers {
 
     //A loop in the code
     //Parsed by parsing the code inside the loop
-    def loop: Parser[Operation] =  "[" ~ block ~ "]" ^^ {
-        case l:(~[String,~[List[Operation],String]]) => {
-            val start = StartLoopOperation()
-            val end = EndLoopOperation()
-            start.set(end)
-            end.set(start)
-            start :: l._2._1 :: end
-        }
+    def loop: Parser[Operation] = ("[" ~ block ~ "]") ^^ {
+        case "[" ~ operations ~ "]" => new LoopOperations(operations)
     }
 }
--- scala/src/TestExecutor.scala
+++ scala/src/TestExecutor.scala
@@ -1,14 +1,11 @@
+
 object TestExecutor {
     def main(args: Array[String]) {
         val parser = new Parser()
 
         // Prints "#"
         var executor = new Interpreter()
-        println("Parsing program 1...")
-        var programOutput: List[List[Operation]] = parser.parse("++[++]++").get
-
-        println("Parsing program 2...")
-        programOutput = parser.parse("+++++[>+++++[>++<-]<-]>>.").get
+        val programOutput: List[List[Operation]] = parser.parse("+++++[>+++++[>++<-]<-]>>.").get
         executor.runProgram(programOutput)
         // Prints "Hello World!\n"
         executor = new Interpreter()
