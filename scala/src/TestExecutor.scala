7b5e133f114ce880006fec9c84865adb14a72aba
William Morriss <wjmelements@gmail.com>
2013-11-21 20:10:45 -0600

    Hello World (not working)

--- scala/src/TestExecutor.scala
+++ scala/src/TestExecutor.scala
@@ -1,13 +1,18 @@
 object TestExecutor {
     def main(args: Array[String]) {
         val parser = new Parser()
-        val executor = new Interpreter()
 
         // Prints "#"
+        var executor = new Interpreter()
         val programOutput:List[Operation] = parser.parse("+++++[>+++++++<-]>.").get
         executor.runProgram(programOutput)
+        // Prints "Hello World!\n"
+        executor = new Interpreter()
+        val programHello:List[Operation] = parser.parse("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.").get
+        executor.runProgram(programHello)
 
         // Prints input char
+        executor = new Interpreter()
         val programInput:List[Operation] = parser.parse(",>,<.>.").get
         print("\nEnter 2 chars: ")
         executor.runProgram(programInput)
