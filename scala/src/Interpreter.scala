a10a1f2db7657866bf70c709388c14a748a8545f
Chris de la Iglesia <cmdli@utexas.edu>
2013-11-22 15:10:54 -0600

    Reverted correctly this time

--- scala/src/Interpreter.scala
+++ scala/src/Interpreter.scala
@@ -1,3 +1,4 @@
+
 import java.util.concurrent.atomic.AtomicInteger
 import scala.actors.Actor
 
@@ -20,95 +21,64 @@ class Interpreter {
         t.start()
         t.join()
         dataArr
-}
-
-    def runLine(line: String) {
-        var index = 0
-        while(index < line.length){
-            line(index) match {
-            case "+" => dataArr(dataPointer).incrementAndGet
-            case "-" => dataArr(dataPointer).decrementAndGet
-            case "." => print(dataArr(dataPointer).get.toChar)
-            case "," => dataArr(dataPointer).set(Console.in.read)
-            case ">" => dataPointer += 1
-            case "<" => dataPointer -= 1
-            case "[" => if(dataArr(dataPointer) == 0) index = matchEnd(line, index)
-            case "]" => if(dataArr(dataPointer) != 0) index = matchStart(line, index)
-            }
-            index += 1
-        }
     }
 
-    //Run one operation
-    def runOp(op: Operation): Unit = op match {
-        case AddOperation() => add()
-        case SubOperation() => subtract()
-        case PrintOperation() => printData()
-        case InputOperation() => scan()
-        case ShiftRightOperation() => shiftRight()
-        case ShiftLeftOperation() => shiftLeft()
-        case s:StartLoopOperation => startLoop(s)
-        case e:EndLoopOperation => endLoop(e)
-        case ForkOperation() => fork()
-        case InvalidOperation() => ()
-    }
+    class Process(program: List[List[Operation]], index: Int) extends Runnable {
 
-    def matchEnd(line: String, from:Int):Int {
-        var index:Int = from
-        var num:Int = 0
-        while(index < line.length) {
-            if(line.charAt(index) == '[')
-                num += 1
-            if(line.charAt(index) == ']') {
-                if(num == 0)
-                    return index
-                num -= 1
-            }
-            index += 1
-        }
-        return -1
-    }
+        var children: List[Thread] = null
 
-    def matchStart(line: String, from:Int):Int {
-        var index:Int = from
-        var num:Int = 0
-        while(index >= 0) {
-            if(line.charAt(index) == ']')
-                num += 1
-            if(line.charAt(index) == '[') {
-                if(num == 0)
-                    return index
-                num -= 1
-            }
-            index -= 1
-        }
-        return -1
-    }
-
-    def add() = dataArr(dataPointer).incrementAndGet
-
-    def subtract() = dataArr(dataPointer).decrementAndGet
-
-    def printData() = print(dataArr(dataPointer).get.toChar)
-
-    def scan() = dataArr(dataPointer).set(Console.in.read)
-
-    def shiftRight() = dataPointer += 1
-
-    def shiftLeft() = dataPointer -= 1
-
-    //Run a loop by running the code inside of it while data is zero
-    /*def loop(loopOperations: List[Operation]) = {
-        while (dataArr(dataPointer).get != 0) {
-            for (op: Operation <- loopOperations) {
+        def run() {
+            for (op: Operation <- program(index)) {
                 runOp(op)
             }
+            if(children != null) {
+                for(child: Thread <- children)
+                    child.join()
+            }
         }
-    }*/
+
+        //Run one operation
+        def runOp(op: Operation): Unit = op match {
+            case AddOperation() => add()
+            case SubOperation() => subtract()
+            case PrintOperation() => printData()
+            case InputOperation() => scan()
+            case ShiftRightOperation() => shiftRight()
+            case ShiftLeftOperation() => shiftLeft()
+            case LoopOperations(operations) => loop(operations)
+            case ForkOperation() => fork()
+            case InvalidOperation() => ()
+        }
+
+        def add() = dataArr(dataPointer).incrementAndGet
+
+        def subtract() = dataArr(dataPointer).decrementAndGet
+
+        def printData() = print(dataArr(dataPointer).get.toChar)
+
+        def scan() = dataArr(dataPointer).set(Console.in.read)
+
+        def shiftRight() = dataPointer += 1
+
+        def shiftLeft() = dataPointer -= 1
+
+        //Run a loop by running the code inside of it while data is zero
+        def loop(loopOperations: List[Operation]) = {
+            while (dataArr(dataPointer).get != 0) {
+                for (op: Operation <- loopOperations) {
+                    runOp(op)
+                }
+            }
+        }
+
+        def fork() {
+            var t = new Thread(new Process(program, index+1))
+            t.start()
+        }
+    }
 
 
 
 
 
 }
-
