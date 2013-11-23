b8d12487fb9f4e0f69f239793565818840e77f61
William Morriss <wjmelements@gmail.com>
2013-11-23 15:22:22 -0600

    Wrote test for pipe

--- scala/src/TestExecutor.scala
+++ scala/src/TestExecutor.scala
@@ -1,28 +1,37 @@
-
 object TestExecutor {
     def main(args: Array[String]) {
         val parser = new Parser()
 
-        // Prints "2"
+        // Prints "2", verify loop within loop
         val programOutput: List[List[Operation]] = parser.parse("+++++[>+++++[>++<-]<-]>>.").get
         var executor = new Interpreter(programOutput)
         executor.runProgram()
+
         // Prints "Hello World!\n"
         val programHello:List[List[Operation]] = parser.parse("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.").get
         executor = new Interpreter(programHello)
         executor.runProgram()
 
-        // Verify input
+        // Verify input/output
         val programInput:List[List[Operation]] = parser.parse(",>,<.>.").get
         executor = new Interpreter(programInput)
         print("\nEnter 2 chars: ")
         executor.runProgram()
         println()
+
         // Verify copy by value
-        val programPara:List[List[Operation]] = parser.parse("+>,>,<<f>....\n>>....\n").get
-        executor = new Interpreter(programPara)
+        val programValue:List[List[Operation]] = parser.parse("+>,>,<<f>....\n>>....").get
+        executor = new Interpreter(programValue)
         print("\nEnter 2 chars: ")
         executor.runProgram()
         println()
+
+        // Verify pi|pe
+        val programPipe:List[List[Operation]] = parser.parse("+>,>,<<f>.|.|.|.\n1234567>>.|.|.|.\n").get
+        executor = new Interpreter(programPipe)
+        print("\nEnter 2 chars: ")
+        executor.runProgram()
+        println()
+
     }
 }
