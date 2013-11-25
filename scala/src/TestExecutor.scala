cdbb5cc764a25bd73f1c93a1072d8322df3922df
William Morriss <wjmelements@gmail.com>
2013-11-25 15:48:03 -0600

    whitespace, new test, cleaned up tests

--- scala/src/TestExecutor.scala
+++ scala/src/TestExecutor.scala
@@ -2,49 +2,66 @@ object TestExecutor {
     def main(args: Array[String]) {
         val parser = new Parser()
 
+
         // Prints "2", verify loop within loop
         val programOutput: List[List[Operation]] = parser.parse("+++++[>+++++[>++<-]<-]>>.").get
         var executor = new Interpreter(new Program(programOutput))
+        println("Expected: 2")
         executor.runProgram()
+        println
 
         // Prints "Hello World!\n"
         val programHello:List[List[Operation]] = parser.parse("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.").get
         executor = new Interpreter(new Program(programHello))
+        println("\nExpected: Hello World!\\n")
         executor.runProgram()
+        println
 
         // Verify input/output
         val programInput:List[List[Operation]] = parser.parse(",>,<.>.").get
         executor = new Interpreter(new Program(programInput))
-        print("\nEnter 2 chars: ")
+        println("\nExpected: The first two characters you entered")
+        print("Enter 6 chars: ")
         executor.runProgram()
         println()
 
         // Verify copy by value
         val programValue:List[List[Operation]] = parser.parse("+>,>,<<*>....\n>>....").get
         executor = new Interpreter(new Program(programValue))
-        print("\nEnter 2 chars: ")
+        println("\nExpected: eight characters")
         executor.runProgram()
         println()
 
         // Verify pipe
         val programPipe:List[List[Operation]] = parser.parse("+>,>,<<*>.|.|.|.\n1234567>>.|.|.|.\n").get
         executor = new Interpreter(new Program(programPipe))
-        print("\nEnter 2 chars: ")
+        println("\nExpected: four adjacent pairs of the same characters")
         executor.runProgram()
         println()
 
-	// Print "ABCDEF" without threads
- 	val programPrintSequence:List[List[Operation]] = parser.parse("+++++[>+++++++++++++<-]>-<++++++[>+.<-]").get
-        executor = new Interpreter(new Program(programPrintSequence))
-        print("\nEnter 2 chars: ")
-        executor.runProgram()
+        // Print "ABCDEF" without threads
+         val programPrintSequence:List[List[Operation]] = parser.parse("+++++[>+++++++++++++<-]>-<++++++[>+.<-]").get
+            executor = new Interpreter(new Program(programPrintSequence))
+            println("\nExpected: ABCDEF")
+            executor.runProgram()
+            println()
+
+        // Prints "ABCDEF" using threads and syncing between threads within a loop
+        val programLoopSync:List[List[Operation]] = parser.parse("+*++++[>+++++++++++++<-]>-<++++++|[>+|m<-]\n012345678901234567890123456789012|[>m|,<-]").get
+        executor = new Interpreter(new Program(programLoopSync))
+        //executor.runProgram()
         println()
 
-	// Prints "ABCDEF" using threads and syncing between threads within a loop
-	val programLoopSync:List[List[Operation]] = parser.parse("+*++++[>+++++++++++++<-]>-<++++++|[>+|m<-]\n012345678901234567890123456789012|[>m|,<-]").get
-	executor = new Interpreter(new Program(programLoopSync))
-	//executor.runProgram()
-	println()
-
+        // Prints input character 36 times and verifies that we can clone concurrently without error
+        executor = new Interpreter(
+            new Program(
+                parser.parse(
+                    "+++++++[>+++++++<-]>-->+*\n******\n******\n<+."
+                ).get
+            )
+        )
+        println("Expected: 36 characters ending with S")
+        executor.runProgram()
+        println
     }
 }
